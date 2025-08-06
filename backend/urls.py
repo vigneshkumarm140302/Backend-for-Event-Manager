d5fca3b9332fea5d1cbee08eafdc4f3606f920e1
VIGNESHKUMAR M <vigneshkumarm140302@gmail.com>
2025-08-06 15:52:43 +0530

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -5,8 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('Backend-for-Event-Manager/admin/', admin.site.urls),
+    path('Backend-for-Event-Manager/api/', include('api.urls')),
+    path('Backend-for-Event-Manager/api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('Backend-for-Event-Manager/api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
