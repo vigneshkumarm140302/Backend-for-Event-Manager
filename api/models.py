06a95e5f741418595d559c09a1f06114a76b5158
vigneshkumarm140302 <vigneshkumarm140302@gmail.com>
2025-08-07 20:19:16 +0530

    create models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Daily_task(models.Model):
+    task = models.TextField()
+    date = models.DateTimeField()
+    compleated = models.BooleanField(default=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_daily_task')
+
+    def __str__(self):
+        return self.author
+    
+class Long_term_goals(models.Model):
+    task = models.TextField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    compleated = models.BooleanField(default=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_long_term_goals')
+
+    def __str__(self):
+        return self.author
